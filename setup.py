c36431f1c6e7602f2f782a5fb720aae47ff84da8
Antoine Pitrou <solipsis@pitrou.net>
2012-01-29 23:17:08 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from distutils.core import setup
 
 if sys.version_info <= (3, 2):
-    sys.exit("Python 3.2 or later needed")
+    sys.exit("Python 3.2 or later required")
 
 setup(
     name='pathlib',
@@ -14,5 +14,15 @@ setup(
     #long_description=open('README.txt').read(),
     author='Antoine Pitrou',
     author_email='solipsis@pitrou.net',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Filesystems',
+        ],
     #url='http://walkdir.readthedocs.org',
 )
