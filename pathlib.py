8b2c98579e76bca1abd78a86ae29ab8e9c5c3507
Antoine Pitrou <solipsis@pitrou.net>
2013-11-22 16:47:36 +0100

    EEXIST needn't be imported

--- pathlib.py
+++ pathlib.py
@@ -15,7 +15,7 @@ except ImportError:
 
 from collections import Sequence, defaultdict
 from contextlib import contextmanager
-from errno import EINVAL, ENOENT, EEXIST
+from errno import EINVAL, ENOENT
 from itertools import chain, count
 from operator import attrgetter
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
