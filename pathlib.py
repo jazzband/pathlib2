3ab70ad0b70f1588e53936cd1b5c45fc306abc19
Antoine Pitrou <solipsis@pitrou.net>
2013-12-10 20:29:35 +0100

    Backport cd8230437f40

--- pathlib.py
+++ pathlib.py
@@ -947,6 +947,13 @@ class Path(PurePath):
         # A stub for the opener argument to built-in open()
         return self._accessor.open(self, flags, mode)
 
+    def _raw_open(self, flags, mode=0o777):
+        """
+        Open the file pointed by this path and return a file descriptor,
+        as os.open() does.
+        """
+        return self._accessor.open(self, flags, mode)
+
     # Public API
 
     @classmethod
@@ -1044,13 +1051,6 @@ class Path(PurePath):
         """
         import grp
         return grp.getgrgid(self.stat().st_gid).gr_name
-
-    def _raw_open(self, flags, mode=0o777):
-        """
-        Open the file pointed by this path and return a file descriptor,
-        as os.open() does.
-        """
-        return self._accessor.open(self, flags, mode)
 
     def open(self, mode='r', buffering=-1, encoding=None,
              errors=None, newline=None):
