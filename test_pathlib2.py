16d4aa6b4dbd5bb76cd18ae2ec4418f0ed1d18b1
scienceopen <scienceopen@users.noreply.github.com>
2015-11-04 19:06:10 -0500

    selftest

--- test_pathlib2.py
+++ test_pathlib2.py
@@ -1420,13 +1420,22 @@ class _BasePathTest(object):
     def test_open_common(self):
         p = self.cls(BASE)
         with (p / 'fileA').open('r') as f:
-            self.assertIsInstance(f, io.TextIOBase)
+            if six.PY2:
+                self.assertIsInstance(f,file)
+            else:
+                self.assertIsInstance(f, io.TextIOBase)
             self.assertEqual(f.read(), "this is file A\n")
         with (p / 'fileA').open('rb') as f:
-            self.assertIsInstance(f, io.BufferedIOBase)
+            if six.PY2:
+                self.assertIsInstance(f,file)
+            else:
+                self.assertIsInstance(f, io.BufferedIOBase)
             self.assertEqual(f.read().strip(), b"this is file A")
         with (p / 'fileA').open('rb', buffering=0) as f:
-            self.assertIsInstance(f, io.RawIOBase)
+            if six.PY2:
+                self.assertIsInstance(f,file)
+            else:
+                self.assertIsInstance(f, io.RawIOBase)
             self.assertEqual(f.read().strip(), b"this is file A")
 
     def test_read_write_bytes(self):
