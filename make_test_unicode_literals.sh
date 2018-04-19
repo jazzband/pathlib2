a9f48d75efa39e30b2337bdfc06850db5398fff4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-19 09:01:30 +0100

    Also need to override six.u.

--- make_test_unicode_literals.sh
+++ make_test_unicode_literals.sh
@@ -2,3 +2,4 @@
 
 echo "from __future__ import unicode_literals" > tests/test_unicode_literals.py
 cat tests/test_pathlib2.py >> tests/test_unicode_literals.py
+sed -i 's/import six/import six; six.u = lambda x: x/' tests/test_unicode_literals.py
