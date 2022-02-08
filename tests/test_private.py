44f00dbe4372d9d88c0f145df5b4f51061e1d50c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2022-02-08 13:59:22 +0000

    Drop Python 2.7. (#75)
    Syncs with latest pathlib from cpython b5527688aae11d0b5af58176267a9943576e71e5.

---
+++ tests/test_private.py
@@ -0,0 +1,28 @@
+# some extra tests for coverage
+
+import pytest
+import os
+from pathlib2 import os_path_realpath
+
+
+@pytest.mark.skipif(os.name != "nt", reason="Windows only test")
+def test_realpath_nt_nul():
+    assert os_path_realpath(b'nul') == b'\\\\.\\NUL'
+    assert os_path_realpath('nul') == '\\\\.\\NUL'
+
+
+@pytest.mark.skipif(os.name != "nt", reason="Windows only test")
+def test_realpath_nt_unc():
+    assert os_path_realpath('\\\\?\\UNC\\localhost\\C$') == '\\\\?\\UNC\\localhost\\C$'
+
+
+@pytest.mark.skipif(os.name != "nt", reason="Windows only test")
+def test_realpath_nt_badpath():
+    with pytest.raises(FileNotFoundError):
+        os_path_realpath('\\\\invalid\\server')
+    with pytest.raises(FileNotFoundError):
+        os_path_realpath('does/not/exist', strict=True)
+
+
+def test_realpath_bytes():
+    assert os_path_realpath(b'abc.xyz').endswith(b'abc.xyz')
